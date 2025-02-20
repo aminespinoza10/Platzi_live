e2ef24652ce1044a05283abae424340d0e6a873b
aminespinoza10 <amin.espinoza@outlook.com>
2025-02-20 16:13:01 -0600

    Project updated

--- src/main.tf
+++ src/main.tf
@@ -3,3 +3,80 @@ resource "azuredevops_project" "project" {
   description = "This is a project template that is used to create the same project structure across different teams"
 }
 
+resource "azuredevops_dashboard" "azdo_dashboard" {
+  project_id = azuredevops_project.project.id
+  name       = "Metrics dashboard"
+}
+
+resource "azuredevops_git_repository" "azdo_code_repo" {
+  project_id     = azuredevops_project.project.id
+  name           = "Code"
+  default_branch = "refs/heads/main"
+  initialization {
+    init_type = "Clean"
+  }
+  lifecycle {
+    ignore_changes = [
+      initialization
+    ]
+  }
+}
+
+resource "azuredevops_git_repository" "azdo_infra_repo" {
+  project_id     = azuredevops_project.project.id
+  name           = "Infrastructure"
+  default_branch = "refs/heads/main"
+  initialization {
+    init_type = "Clean"
+  }
+  lifecycle {
+    ignore_changes = [
+      initialization
+    ]
+  }
+}
+
+resource "azuredevops_build_definition" "azdo_pipeline" {
+  project_id = azuredevops_project.project.id
+  name       = "Code Continous Integration"
+  path       = "\\.azdo"
+
+  ci_trigger {
+    use_yaml = true
+  }
+
+  schedules {
+    branch_filter {
+      include = ["main"]
+      exclude = ["test", "regression"]
+    }
+    days_to_build              = ["Wed", "Sun"]
+    schedule_only_with_changes = true
+    start_hours                = 10
+    start_minutes              = 59
+    time_zone                  = "(UTC) Coordinated Universal Time"
+  }
+
+  repository {
+    repo_type   = "TfsGit"
+    repo_id     = azuredevops_git_repository.azdo_code_repo.id
+    branch_name = azuredevops_git_repository.azdo_code_repo.default_branch
+    yml_path    = "code-ci.yml"
+  }
+
+  variable {
+    name  = "PipelineVariable"
+    value = "Go Microsoft!"
+  }
+
+  variable {
+    name         = "PipelineSecret"
+    secret_value = "ZGV2cw"
+    is_secret    = true
+  }
+}
+
+resource "azurerm_resource_group" "azure_rg" {
+  name = "rg-platzi-template"
+  location = "East US 2"
+}
--- src/provider.tf
+++ src/provider.tf
@@ -4,10 +4,19 @@ terraform {
       source  = "microsoft/azuredevops"
       version = ">=0.1.0"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
   }
 }
 
 provider "azuredevops" {
   org_service_url = "https://dev.azure.com/${var.azure_devops_organization}"
   personal_access_token = var.azure_devops_pat
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "Azure Subscription ID"
 }
